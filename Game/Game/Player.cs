9be1c800e08da5bdce8020db3efb6f0a25c49a01
YoiOni <Daratars@gmail.com>
2021-06-12 00:08:51 +0500

    Создал два класса(ну это так помомечка), добавил спрайты в папочку

---
+++ Game/Game/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Player
+    {
+
+        public int Health { get; set; }
+        public int Armor { get; set; }
+        public Player()
+        {
+            Health = 100;
+            Armor = 100;
+        }
+
+    }
+}
