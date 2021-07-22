74ab0021d52e043b51d15d6c0b487044bc8b2fda
Lanoste <tarasova.son@gmail.com>
2021-07-22 17:47:26 +0500

    Введение класса кобра, изменение в закрытие файла и открытии бэкграундов

---
+++ Game/Game/Cobra.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Game
+{
+    class Cobra
+    {
+        public int PosX { get; set; }
+        private int frame = 0;
+        private bool flip;
+        public int point;
+        public string Status { get; set; }
+        public bool OnMove { get; set; }
+        public int PosY { get; set; }
+        public int Health { get; set; }
+        public int Att { get; set; }
+        public int Armor { get; set; }
+        public string Direction { get; set; }
+        public int Size { get; set; }
+
+        private Image Idle;
+        private Image Model;
+        private Image Sprite;
+        public Dictionary<string, List<Image>> Animations { get; set; }
+
+        public Cobra()
+        {
+            Health = 100;
+            Armor = 10;
+            point = 10;
+            Att = 15;
+            Sprite = Sprites.Sprites.Cobra;
+            Size = 100;
+            PosX = 11000;
+            PosY = 11000;
+            Animations = new Dictionary<string, List<Image>>();
+            Animations.Add("Idle", SetAnimation(8, 0));
+            Animations.Add("Run", SetAnimation(8, 1));
+            Animations.Add("Attack", SetAnimation(6, 2));
+            Animations.Add("Death", SetAnimation(6, 4));
+            Idle = Animations["Idle"][0];
+            Model = Idle;
+            Move();
+        }
+        private List<Image> SetAnimation(int animFrames, int row, bool flip = false)
+        {
+            var frames = new List<Image>();
+            Image part;
+            Graphics g;
+            for (int i = 0; i < animFrames; i++)
+            {
+                part = new Bitmap(96, 96);
+                g = Graphics.FromImage(part);
+                g.DrawImage(Sprite, new Rectangle(new System.Drawing.Point(0, 0), new Size(Size, Size)), 98 * i, 96 * row, 96, 96, GraphicsUnit.Pixel);
+                frames.Add(new Bitmap(part));
+            }
+            return frames;
+        }
+        public void Move()
+        {
+            Player player = new Player();
+            if (Math.Abs(player.PosX - PosX) > 150 && Math.Abs(player.PosX - PosX) < 20)
+            {
+                if ((player.PosX - PosX) > 150) Direction = "Left";
+                else Direction = "Right";
+                OnMove = true;
+                Status = "Run";
+            }
+            else OnMove = false;
+            if (Health == 0)
+            {
+                OnMove = false;
+                Status = "Death";
+            }
+            if (Math.Abs(player.PosX - PosX) <= 20)
+            {
+                if (Direction == "Idle" && OnMove)
+                    OnMove = true;
+                Direction = "Idle";
+                Status = "Attack";
+            }
+        }
+
+        public void PlayAnimation(Graphics g) => g.DrawImage(ModelStatus(), PosX, PosY);
+        private Image ModelStatus()
+        {
+            Bitmap newFrame = new Bitmap(Model);
+
+            if (OnMove == true)
+            {
+                if (Direction == "Left")
+                {
+                    PosX -= 10;
+                    flip = true;
+                }
+                if (Direction == "Right")
+                {
+                    PosX += 10;
+                    flip = false;
+                }
+                newFrame = new Bitmap(Animations[Status][frame]);
+                frame++;
+                if (frame >= Animations[Status].Count) frame = 0;
+            }
+            if (OnMove == false)
+            {
+                frame = 0;
+                Status = "Idle";
+                newFrame = new Bitmap(Idle);
+            }
+            if (flip) newFrame.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            return Model = newFrame;
+        }
+    }
+}
--- Game/Game/Form1.cs
+++ Game/Game/Form1.cs
@@ -17,6 +17,7 @@ namespace Game
     public partial class Form1 : Form
     {
         Player player;
+        Cobra cobra;
         new readonly List<Keys> Controls = new List<Keys>()
         {
             //Keys.Up,
@@ -39,8 +40,6 @@ namespace Game
             KeyDown += new KeyEventHandler(Press);
             KeyUp += new KeyEventHandler(Unpress);
             this.BackgroundImage = Resources.StartLock;
-            //this.BackgroundImage = levels.NextLocation();
-            //this.BackgroundImage = Image.FromFile("C:\\Users\\Admin\\Source\\Repos\\Hikozume\\Game\\Game\\Game\\Resources\\StartLock.jpg");
             this.BackgroundImageLayout = ImageLayout.Stretch;
             Initialization();
         }
@@ -50,24 +49,42 @@ namespace Game
             MenuForm menu = new MenuForm();
             menu.ShowDialog();
             player = new Player();
+            cobra = new Cobra();
+            cobra.PosX = 500;
+            cobra.PosY = Screen.PrimaryScreen.Bounds.Size.Height - 70;
         }
 
 
         private void OnPaint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            //var model = player.Model;
-            //Image part = new Bitmap(96, 96);
-            //Graphics g = Graphics.FromImage(part);
-            //var list = new List<Image>();
-            //g.DrawImage(player.Model, new Rectangle(new Point(0,0), new Size(100, 100)), 0, 0, 96, 96, GraphicsUnit.Pixel);
-            //list.Add(part);
-
             player.PlayAnimation(g);
+            cobra.PlayAnimation(g);
             timer1.Start();
             // graph.DrawImage(player.Model, new Rectangle(new Point(50,50),new Size(100,100)),0,0,96,96,GraphicsUnit.Pixel);
         }
-
+        // Может быть реализовать таймер сюда чтобы показовало снесенное ХП
+        //Label HP = new Label()
+        //{
+        //    Size = new Size(25, 25)
+        //};
+        //public bool MessageHP()
+        //{
+        //    if (Math.Abs(player.PosX - cobra.PosX) <= 20)
+        //    {
+        //        HP.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, 204);
+        //        HP.Location = new System.Drawing.Point(cobra.PosX+10, cobra.PosY+10);
+        //        HP.Text = "-10";
+        //        base.Controls.Add(HP);
+        //        HP.Visible = true;
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        HP.Visible = false;
+        //        return false;
+        //    }
+        //}
         public void Press(object sender, KeyEventArgs e)
         {
             MesageToExit();
@@ -96,7 +113,17 @@ namespace Game
                     //load.Hide();
                 }
             }
-
+            if (e.KeyCode == Keys.Space)
+            {
+                player.OnMove = true;
+                player.Direction = "Idle";
+                player.Status = "Attack";
+                if (Math.Abs(player.PosX - cobra.PosX) <= 20 && cobra.Health >= 0)
+                {
+                    cobra.Health -= 10;
+                }
+                if (cobra.Health <= 0) p.point += cobra.point;
+            }
         }
 
         public void Unpress(object sender, KeyEventArgs e)
@@ -121,7 +148,7 @@ namespace Game
             if (player.PosX >= Size.Width - 350)
             {
                 LM.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, 204);
-                LM.Location = new Point(this.Size.Width - 100, this.Size.Height - 100);
+                LM.Location = new System.Drawing.Point(this.Size.Width - 100, this.Size.Height - 100);
                 LM.Text = "F";
                 base.Controls.Add(LM);
                 LM.Visible = true;
